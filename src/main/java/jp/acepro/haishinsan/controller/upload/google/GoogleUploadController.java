7dd005b08a0c689fe06f4514b39594857d2f841a
Tienan Liu <ryu.tetsuo@sparkworks.co.jp>
2019-07-27 18:57:55 +0900

    update: [upload]-[google]-[adTypeSelection]-[xxxxAdCreate]

--- src/main/java/jp/acepro/haishinsan/controller/upload/google/GoogleUploadController.java
+++ src/main/java/jp/acepro/haishinsan/controller/upload/google/GoogleUploadController.java
@@ -2,9 +2,14 @@ package jp.acepro.haishinsan.controller.upload.google;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import jp.acepro.haishinsan.form.UploadGoogleTextAdCreateForm;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 @RequestMapping("/upload/google")
 public class GoogleUploadController {
@@ -23,6 +28,12 @@ public class GoogleUploadController {
 		return mv;
 	}
 
+	@PostMapping("/bannerAd/confirm")
+	public ModelAndView bannerAdConfirm(UploadGoogleTextAdCreateForm form) {
+		log.debug(form.toString());
+		return null;
+	}
+
 	// Create Banner&Text Ad
 	@GetMapping("/bannerTextAd/create")
 	public ModelAndView bannerTextAdCreate(ModelAndView mv) {
@@ -33,8 +44,16 @@ public class GoogleUploadController {
 	// Create Text Ad
 	@GetMapping("/textAd/create")
 	public ModelAndView textAdCreate(ModelAndView mv) {
+		UploadGoogleTextAdCreateForm form = new UploadGoogleTextAdCreateForm();
 		mv.setViewName("upload/google/textAd/create");
+		mv.addObject("form", form);
 		return mv;
+	}
+
+	@PostMapping("/textAd/confirm")
+	public ModelAndView textAdConfirm(UploadGoogleTextAdCreateForm form) {
+		log.debug(form.toString());
+		return null;
 	}
 
 }
