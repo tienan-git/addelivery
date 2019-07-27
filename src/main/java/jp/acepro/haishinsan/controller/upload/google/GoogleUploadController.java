95615dc337062f86920b2f3de511247b92da4a73
Tienan Liu <ryu.tetsuo@sparkworks.co.jp>
2019-07-27 16:45:03 +0900

    update: [upload]-[google]-[adTypeSelection]-[xxxxAdCreate]

--- src/main/java/jp/acepro/haishinsan/controller/upload/google/GoogleUploadController.java
+++ src/main/java/jp/acepro/haishinsan/controller/upload/google/GoogleUploadController.java
@@ -9,9 +9,32 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/upload/google")
 public class GoogleUploadController {
 
+	// Select Ad Type
 	@GetMapping("/adTypeSelection")
 	public ModelAndView adTypeSelection(ModelAndView mv) {
 		mv.setViewName("upload/google/adTypeSelection");
 		return mv;
 	}
+
+	// Create Banner Ad
+	@GetMapping("/bannerAd/create")
+	public ModelAndView bannerAdCreate(ModelAndView mv) {
+		mv.setViewName("upload/google/bannerAd/create");
+		return mv;
+	}
+
+	// Create Banner&Text Ad
+	@GetMapping("/bannerTextAd/create")
+	public ModelAndView bannerTextAdCreate(ModelAndView mv) {
+		mv.setViewName("upload/google/bannerTextAd/create");
+		return mv;
+	}
+
+	// Create Text Ad
+	@GetMapping("/textAd/create")
+	public ModelAndView textAdCreate(ModelAndView mv) {
+		mv.setViewName("upload/google/textAd/create");
+		return mv;
+	}
+
 }
