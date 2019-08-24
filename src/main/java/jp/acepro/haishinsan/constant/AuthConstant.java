500000dff64eac12bbdb814942016f27aa3126a3
sohrichoh <sohrichoh@sourichounombp>
2019-08-24 14:42:56 +0900

    権限定数変更

--- src/main/java/jp/acepro/haishinsan/constant/AuthConstant.java
+++ src/main/java/jp/acepro/haishinsan/constant/AuthConstant.java
@@ -10,80 +10,80 @@ public class AuthConstant {
 	/*---------------------
 	 * アカウント体制管理
 	 ---------------------*/
-	/** 法人管理 */
-	public static final String CORPORATION_MANAGE = "	CORPORATION_MANAGE	";
-	/** 法人照会 */
-	public static final String CORPORATION_VIEW = "	CORPORATION_VIEW	";
-	/** 店舗管理 */
-	public static final String SHOP_MANAGE = "	SHOP_MANAGE	";
-	/** 店舗照会 */
-	public static final String SHOP_VIEW = "	SHOP_VIEW	";
-	/** ユーザー管理 */
-	public static final String USER_MANAGE = "	USER_MANAGE	";
-	/** ユーザー照会 */
-	public static final String USER_VIEW = "	USER_VIEW	";
-	/** 代理店管理 */
-	public static final String AGENCY_MANAGE = "	AGENCY_MANAGE	";
-	/** 代理店照会 */
-	public static final String AGENCY_VIEW = "	AGENCY_VIEW	";
-	/** キャンペーン承認 */
-	public static final String CAMPAIGN_APPROVAL = "	CAMPAIGN_APPROVAL	";
-	/** ショップリスト_管理 */
-	public static final String SHOPLIST_MANAGE = "	SHOPLIST_MANAGE	";
-	/** ショップリスト_代理店 */
-	public static final String SHOPLIST_AGENCY = "	SHOPLIST_AGENCY	";
-	/** ショップリスト_法人 */
-	public static final String SHOPLIST_CORPORATION = "	SHOPLIST_CORPORATION	";
-	/** ショップリスト_店舗 */
-	public static final String SHOPLIST_SHOP = "	SHOPLIST_SHOP	";
+	/** 法人 法人管理 */
+	public static final String CORPORATION_MANAGE = "CORPORATION_MANAGE";
+	/** 法人 法人照会 */
+	public static final String CORPORATION_VIEW = "CORPORATION_VIEW";
+	/** 店舗 店舗管理 */
+	public static final String SHOP_MANAGE = "SHOP_MANAGE";
+	/** 店舗 店舗照会 */
+	public static final String SHOP_VIEW = "SHOP_VIEW";
+	/** ユーザー ユーザー管理 */
+	public static final String USER_MANAGE = "USER_MANAGE";
+	/** ユーザー ユーザー照会 */
+	public static final String USER_VIEW = "USER_VIEW";
+	/** 代理店 代理店管理 */
+	public static final String AGENCY_MANAGE = "AGENCY_MANAGE";
+	/** 代理店 代理店照会 */
+	public static final String AGENCY_VIEW = "AGENCY_VIEW";
+	/** キャンペーン承認 キャンペーン承認 */
+	public static final String CAMPAIGN_APPROVAL = "CAMPAIGN_APPROVAL";
+	/** 店舗管理範囲 ショップリスト_管理 */
+	public static final String SHOPLIST_MANAGE = "SHOPLIST_MANAGE";
+	/** 店舗管理範囲 ショップリスト_代理店 */
+	public static final String SHOPLIST_AGENCY = "SHOPLIST_AGENCY";
+	/** 店舗管理範囲 ショップリスト_法人 */
+	public static final String SHOPLIST_CORPORATION = "SHOPLIST_CORPORATION";
+	/** 店舗管理範囲 ショップリスト_店舗 */
+	public static final String SHOPLIST_SHOP = "SHOPLIST_SHOP";
 
 	/*---------------------
 	 * 広告作成
 	 ---------------------*/
-	/** 新規作成 */
-	public static final String CAMPAIGN_MANAGE = "	CAMPAIGN_MANAGE	";
+	/** 広告作成 新規作成 */
+	public static final String CAMPAIGN_MANAGE = "CAMPAIGN_MANAGE";
 
 	/*---------------------
 	 * 広告物アップロード
 	 ---------------------*/
-	/** 作成 */
-	public static final String CAMPAIGN_UPLOAD = "	CAMPAIGN_UPLOAD	";
+	/** 広告物アップロード 作成 */
+	public static final String CAMPAIGN_UPLOAD = "CAMPAIGN_UPLOAD";
 
 	/*---------------------
 	 * 案件一覧
 	 ---------------------*/
-	/** 登録更新 */
-	public static final String ISSUE_MANAGE = "	ISSUE_MANAGE	";
-	/** 閲覧 */
-	public static final String ISSUE_VIEW = "	ISSUE_VIEW	";
-	/** 分析 */
-	public static final String CAMPAIGN_REPORT_VIEW = "	CAMPAIGN_REPORT_VIEW	";
+	/** 案件一覧 登録更新 */
+	public static final String ISSUE_MANAGE = "ISSUE_MANAGE";
+	/** 案件一覧 閲覧 */
+	public static final String ISSUE_VIEW = "ISSUE_VIEW";
+	/** レポート 分析 */
+	public static final String CAMPAIGN_REPORT_VIEW = "CAMPAIGN_REPORT_VIEW";
 
 	/*---------------------
 	 * 分析レポート
 	 ---------------------*/
-	/** 分析 */
-	public static final String SEGMENT_REPORT_VIEW = "	SEGMENT_REPORT_VIEW	";
+	/** 分析レポート 分析 */
+	public static final String SEGMENT_REPORT_VIEW = "SEGMENT_REPORT_VIEW";
 
 	/*---------------------
 	 * 設定
 	 ---------------------*/
-	/** 閲覧 */
-	public static final String SETTING_MANUAL = "	SETTING_MANUAL	";
-	/** 閲覧 */
-	public static final String SETTING_PROVISION = "	SETTING_PROVISION	";
-	/** 管理 */
-	public static final String SEGMENT_MANAGE = "	SEGMENT_MANAGE	";
-	/** 閲覧 */
-	public static final String SEGMENT_VIEW = "	SEGMENT_VIEW	";
+	/** マニュアル 閲覧 */
+	public static final String SETTING_MANUAL = "SETTING_MANUAL";
+	/** 入稿規定 閲覧 */
+	public static final String SETTING_PROVISION = "SETTING_PROVISION";
+	/** リンク先管理 管理 */
+	public static final String SEGMENT_MANAGE = "SEGMENT_MANAGE";
+	/** リンク先管理 閲覧 */
+	public static final String SEGMENT_VIEW = "SEGMENT_VIEW";
 
 	/*---------------------
 	 * テンプレート設定
 	 ---------------------*/
-	/** 一覧 */
-	public static final String TEMPLATE_VIEW = "	TEMPLATE_VIEW	";
-	/** 新規作成 */
-	public static final String TEMPLATE_MANAGE = "	TEMPLATE_MANAGE	";
+	/** テンプレート管理 一覧 */
+	public static final String TEMPLATE_VIEW = "TEMPLATE_VIEW";
+	/** テンプレート管理 新規作成 */
+	public static final String TEMPLATE_MANAGE = "TEMPLATE_MANAGE";
 
 	/*---------------------
 	 * 以下は不要になる予定
