e6b8b9db892ea7926b95a8545626a11e204ed8c3
soh.richoh <soh.richoh@sparkworks.co.jp>
2019-09-07 22:43:06 +0900

    コメント更新

--- src/main/java/jp/acepro/haishinsan/service/BudgetCalculationService.java
+++ src/main/java/jp/acepro/haishinsan/service/BudgetCalculationService.java
@@ -7,21 +7,17 @@ public interface BudgetCalculationService {
 	/**
 	 * 予算自動計算
 	 * 
-	 * ・終了日時過ぎた場合、予算更新しない
+	 * ・配信中（開始〜終了の間）広告のみ調整する
 	 * 
-	 * ・最小予算以下にならないこと
-	 * 
-	 * ・最大予算は1,000,000超えないこと
-	 * 
-	 * @param startDateTime   開始日時(2019-08-12 12:30)
-	 * @param endDateTime     終了日時(2019-08-16 03:00)
-	 * @param budget          元予算
+	 * @param startDateTime   広告開始日時(2019-08-12 12:30)
+	 * @param endDateTime     広告終了日時(2019-08-16 03:00)
+	 * @param budget          総予算
+	 * @param costFee         実際費用（前日まで使った分）
 	 * @param currentDateTime レポート取得日時
-	 * @param costFee         実際費用（使った分）
-	 * @return 計算後予算
+	 * @return 計算後日予算
 	 */
 
-	long calculateBudget(String startDateTime, String endDateTime, long budget, LocalDateTime currentDateTime,
-			long costFee);
+	long calculateBudget(String startDateTime, String endDateTime, long budget, long costFee,
+			LocalDateTime currentDateTime);
 
 }
--- src/main/java/jp/acepro/haishinsan/service/BudgetCalculationServiceImpl.java
+++ src/main/java/jp/acepro/haishinsan/service/BudgetCalculationServiceImpl.java
@@ -30,8 +30,8 @@ public class BudgetCalculationServiceImpl implements BudgetCalculationService {
 	}
 
 	@Override
-	public long calculateBudget(String startDateTime, String endDateTime, long budget, LocalDateTime currentDateTime,
-			long costFee) {
+	public long calculateBudget(String startDateTime, String endDateTime, long budget, long costFee,
+			LocalDateTime currentDateTime) {
 
 		// TODO 一旦元予算+1のダミーデータ返す
 		return budget + 1;
